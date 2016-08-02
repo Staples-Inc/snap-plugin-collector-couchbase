000936978099c8af3d54cd141b5af631ac88af72
Warren, Jacob <Jacob.Warren@Staples.com>
2016-08-02 16:42:02 -0400

    Updated yaml gen.

--- gen.sh
+++ gen.sh
@@ -2,10 +2,12 @@
 
 metriclist="$(snapctl metric list | cut -f1 | cut -f3- -d"/" | tr "/" .)"
 
-#echo -e "echo: \n$metriclist"
+rm ./sad.yaml
+
+echo "rules:" >> ./sad.yaml
 for line in $metriclist; do
 	foo1="  - pattern: \"%app%.%host%.$line\""
 	foo2="    metric_key: $line"
-	echo $foo1 >> ./sad.yaml
-	echo $foo2 >> ./sad.yaml
+	echo "$foo1" >> ./sad.yaml
+	echo "$foo2" >> ./sad.yaml
 done
